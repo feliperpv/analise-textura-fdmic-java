a104121e11ebd873eb67675c407f5d0233fc2390
Felipe Reis Pavan <feliperpv@gmail.com>
2018-04-16 21:53:39 -0300

    Leitura do arquivo classe.txt

--- src/analisetextura/AnaliseTextura.java
+++ src/analisetextura/AnaliseTextura.java
@@ -1,7 +1,14 @@
 package analisetextura;
 
 import extratorcaracteristicas.FDMIC;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
@@ -15,7 +22,7 @@ public class AnaliseTextura {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //Loading the OpenCV core library  
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
@@ -23,12 +30,12 @@ public class AnaliseTextura {
         //File diretorio_img = new File("C:\\Users\\Cliente\\Desktop\\TCC\\Outex\\Base");
         //Vistex
         File diretorio_img = new File("C:\\Users\\Cliente\\Desktop\\TCC\\Vistex\\Base");
-        //Teste
-        //File diretorio_img = new File("C:\\Users\\Cliente\\Desktop\\TCC\\teste\\Base");
-
+        
         File[] imagens = diretorio_img.listFiles();
         int TAM_DIR = imagens.length;
         FDMIC fdmic = new FDMIC();
+        
+        //Teste
         TAM_DIR = 10;
 
         double matrix[][] = new double[TAM_DIR][66];
@@ -44,10 +51,18 @@ public class AnaliseTextura {
             System.out.println("");
         }
         
-        //Gerar
-        File diretorio_classe = new File("C:\\Users\\Cliente\\Desktop\\TCC\\Vistex");
-        //diretorio_classe
-        
+        BufferedReader classe_txt
+                = new BufferedReader(new FileReader("C:\\Users\\Cliente\\Desktop\\TCC\\Vistex\\classe.txt"));
+
+        List<String> listaClasses = new ArrayList<>();
+
+        String c = classe_txt.readLine();
+        while (c != null) {
+            listaClasses.add(c);
+            c = classe_txt.readLine();
+        }
+
+        classe_txt.close();
     }
 
 }
